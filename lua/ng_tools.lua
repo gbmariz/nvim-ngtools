5fb5f4c3a27c06b505df6d25100a9c5a30d59cfd
Gabriel Mariz <gb.mariz@outlook.com>
2024-03-16 13:18:54 -0300

    :sparkles: (feat): adds opts support

--- lua/ng_tools.lua
+++ lua/ng_tools.lua
@@ -4,9 +4,15 @@ local system = require("utils.system")
 local ng_serve = require("commands.serve")
 local cmd_create = vim.api.nvim_create_user_command
 
-function M.setup()
+function M.setup(opts)
+  opts = opts or {}
+
   -- check that angular/cli is installed and is angular project
   if system.ng_installed() and system.is_ng_project then
+    -- [[ personalized options
+    ng_serve.port = opts.port
+    -- ]]
+
     -- plugin commands
     cmd_create("NgServe", ng_serve.start, { nargs = 0 })
     cmd_create("NgStop", ng_serve.stop, { nargs = 0 })
